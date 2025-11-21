942905aa43a30471c9087d708d7ef82b1a39b6b0
libw2516-oss <libw2516@gmail.com>
2025-11-21 19:29:14 +0800

    normal

---
+++ 6.4_PWM控制舵机/Hardware/Servo.c
@@ -0,0 +1,17 @@
+#include "stm32f10x.h"
+#include "PWM.h"
+
+void Servo_Init(void)
+{
+    PWM_Init();
+
+}
+
+void Servo_SetAngle(float angle)
+{
+    // 舵机的PWM周期为20ms
+    // 角度值范围为-90到+90度
+    // 对应的PWM脉宽范围为0.5ms到2.5ms
+    float compareValue = 1500 + (angle * 2000) / 180; // 1ms + (angle/180)*1ms
+    PWM_SetCompare2(compareValue);
+}
---
+++ 6.4_PWM控制舵机/User/main.c
@@ -0,0 +1,35 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Timer.h"
+#include "Servo.h"
+#include "KEY.h"
+
+uint8_t KeyNum;//可以使用extern在其他文件声明这个变量即可跨文件使用
+float angle = -90;
+
+int main(void){
+
+	OLED_Init();
+	Servo_Init();
+	KEY_Init();
+	Servo_SetAngle(angle);
+	
+while (1)
+{	
+	KeyNum = Key_GetNum();
+	if (KeyNum == 2)
+	{
+		angle += 30;
+		if (angle > 90)
+		{
+			angle = -90;
+		}
+		
+	}
+	Servo_SetAngle(angle);
+}
+
+
+
+}
