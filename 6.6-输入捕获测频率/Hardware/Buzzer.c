0a3fc55785b524999d17fb1d0c9d34c6f3f0a194
libw2516-oss <libw2516@gmail.com>
2025-11-25 15:53:43 +0800

    normal

---
+++ 6.6-输入捕获测频率/Hardware/Buzzer.c
@@ -0,0 +1,34 @@
+#include "stm32f10x.h"
+
+void Buzzer_Init(void)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+};
+
+void Buzzer_On(void)
+{
+    GPIO_ResetBits(GPIOB, GPIO_Pin_12); // Turn on LEDs
+}
+
+void Buzzer_Off(void)
+{
+    GPIO_SetBits(GPIOB, GPIO_Pin_12); 
+}
+
+void Buzzer_Turn(void)
+{
+    if (GPIO_ReadOutputDataBit(GPIOA, GPIO_Pin_12) == 0)
+    {
+        GPIO_SetBits(GPIOB, GPIO_Pin_12); 
+    }
+    else
+    {
+        GPIO_ResetBits(GPIOB, GPIO_Pin_12); 
+    }
+    
+}
